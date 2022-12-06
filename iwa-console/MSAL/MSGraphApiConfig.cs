28c940c0cafda675c86e3c14c024da3a114cd820
Michael Mitchell-Halter <v-michaelmi@microsoft.com>
2022-12-06 12:34:07 -0800

    WAM preview for console app

---
+++ iwa-console/MSAL/MSGraphApiConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iwa_console.MSAL
+{
+    public class MSGraphApiConfig
+    {
+        /// <summary>
+        /// Gets or sets the MS Graph base URL.
+        /// </summary>
+        /// <value>
+        /// The Microsoft Graph base URL.
+        /// </value>
+        public string MSGraphBaseUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scopes for MS graph call.
+        /// </summary>
+        /// <value>
+        /// The scopes as space separated string.
+        /// </value>
+        public string Scopes { get; set; }
+
+        /// <summary>
+        /// Gets the scopes in a format as expected by the various MSAL SDK methods.
+        /// </summary>
+        /// <value>
+        /// The scopes as array.
+        /// </value>
+        public string[] ScopesArray
+        {
+            get
+            {
+                return Scopes.Split(' ');
+            }
+        }
+    }
+}
--- iwa-console/Program.cs
+++ iwa-console/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using iwa_console.MSAL;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Client;
 using System;
 using System.Net.Http;
@@ -33,14 +35,32 @@ namespace iwa_console
 
         private static async Task RunAsync()
         {
-            SampleConfiguration config = SampleConfiguration.ReadFromJsonFile("appsettings.json");
-            var appConfig = config.PublicClientApplicationOptions;
-            var app = PublicClientApplicationBuilder.CreateWithApplicationOptions(appConfig)
-                                                    .Build();
-            var httpClient = new HttpClient();
+            // Using appsettings.json as our configuration settings and utilizing IOptions pattern - https://learn.microsoft.com/dotnet/core/extensions/options
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
-            MyInformation myInformation = new MyInformation(app, httpClient, config.MicrosoftGraphBaseEndpoint);
-            await myInformation.DisplayMeAndMyManagerAsync();
+            // Read configuration
+            AzureADConfig azureADConfig = configuration.GetSection("AzureAD").Get<AzureADConfig>();
+            MSGraphApiConfig msGraphApiConfig = configuration.GetSection("MSGraphApi").Get<MSGraphApiConfig>();
+
+            var msalClientHelper = new MSALClientHelper(azureADConfig);
+
+            var handle = WindowsHelper.GetConsoleOrTerminalWindow();
+            await msalClientHelper.InitializePublicClientAppForWAMBrokerAsync(handle);
+
+            var msGraphHelper = new MSGraphHelper(msGraphApiConfig, msalClientHelper);
+
+            await msGraphHelper.SignInAndInitializeGraphServiceClient();
+
+            var logedInUser = await msGraphHelper.GetMeAsync();
+
+            Console.WriteLine("User information:\n");
+
+            Console.WriteLine($"Display name:\n{ logedInUser.DisplayName ?? "Value not set"}\n");
+            Console.WriteLine($"Employee ID:\n{ logedInUser.EmployeeId  ?? "Value not set"}\n");
+            Console.WriteLine($"Job title:\n{ logedInUser.JobTitle ?? "Value not set"}\n");
+            Console.WriteLine($"Email:\n{ logedInUser.Mail ?? "Value not set"}\n");
+            Console.WriteLine($"Mobile phone:\n{ logedInUser.MobilePhone ?? "Value not set"}\n");
+            Console.WriteLine($"About me:\n{ logedInUser.AboutMe ?? "Value not set"}\n");
         }
     }
 }
